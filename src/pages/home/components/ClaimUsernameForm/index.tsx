e7eaf16f19ca875124ba429c34f454420984ef91
Douglas Scaini <douglasscaini.dev@gmail.com>
2023-01-03 14:45:43 -0300

    Config Prisma

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -2,6 +2,7 @@ import { Button, Text, TextInput } from "@ignite-ui/react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { useRouter } from "next/router";
 import { Form, FormAnnotation } from "./styles";
 
 import { ArrowRight } from "phosphor-react";
@@ -20,13 +21,17 @@ export function ClaimUsernameForm() {
     const {
         register,
         handleSubmit,
-        formState: { errors },
+        formState: { errors, isSubmitting },
     } = useForm<ClaimUsernameFormData>({
         resolver: zodResolver(claimUsernameFormSchema),
     });
 
+    const router = useRouter();
+
     async function handleClaimUsername(data: ClaimUsernameFormData) {
-        console.log(data.username);
+        const { username } = data;
+
+        await router.push(`/register?username=${username}`);
     }
 
     return (
@@ -38,7 +43,7 @@ export function ClaimUsernameForm() {
                     placeholder="seu-usuario"
                     {...register("username")}
                 />
-                <Button size="sm" type="submit">
+                <Button size="sm" type="submit" disabled={isSubmitting}>
                     Reservar
                     <ArrowRight />
                 </Button>
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -2,6 +2,8 @@ import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Container, Form, FormError, Header } from "./styles";
 
 import { ArrowRight } from "phosphor-react";
@@ -21,10 +23,19 @@ export default function Register() {
     const {
         register,
         handleSubmit,
+        setValue,
         formState: { errors, isSubmitting },
     } = useForm<RegisterFormData>({
         resolver: zodResolver(registerFormSchema),
     });
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.query.username) {
+            setValue("username", String(router.query.username));
+        }
+    }, [router.query?.username, setValue]);
 
     async function handleRegister(data: RegisterFormData) {
         console.log(data);
